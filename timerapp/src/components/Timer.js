fa3d7329dcac8f71ceaa25b126a732a3762462e7
shruti166 <shruti.chemistry.lm@gmail.com>
2023-02-23 13:41:29 +0530

    ready to deploy

--- timerapp/src/components/Timer.js
+++ timerapp/src/components/Timer.js
@@ -1,14 +1,30 @@
 import React from "react";
 import "../App.css";
 
-
-export default function Timer({ hours, min, sec}) {
-  
-  return <div className="timer">
-    <div><p>{hours}</p></div>
-    <div><span> : </span></div>
-    <div><p>{min}</p></div>
-    <div><span> : </span></div>
-    <div><p>{sec}</p></div>
-  </div>;
+export default function Timer({ day, hours, min, sec }) {
+  return (
+    <div className="main">
+      <div className="day">
+        <h3>Hello there ! It's {day}</h3>
+      </div>
+      <div className="timer">
+        <div>
+          <p>{hours}</p>
+        </div>
+        <div>
+          <span> : </span>
+        </div>
+        <div>
+          <p>{min}</p>
+        </div>
+        <div>
+          <span> : </span>
+        </div>
+        <div>
+          <p>{sec}</p>
+        </div>
+      </div>
+      
+    </div>
+  );
 }
