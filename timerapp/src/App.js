093a96158395d78c9b36f780e3765ad32df2d4ae
shruti166 <shruti.chemistry.lm@gmail.com>
2023-02-23 13:42:30 +0530

    build

--- timerapp/src/App.js
+++ timerapp/src/App.js
@@ -1,15 +1,31 @@
-import { useState } from 'react';
-import Timer from './components/Timer';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import Timer from "./components/Timer";
+import "./App.css";
+const date = new Date();
 function App() {
-    const [days, setDays] = useState(10);
-    const [hours, setHours] = useState(10);
-    const [min, setMin] = useState(10);
-    const [sec, setSec] = useState(10);
+  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const [hours, setHours] = useState(date.getHours());
+  const [min, setMin] = useState(date.getMinutes());
+  const [sec, setSec] = useState(date.getSeconds());
+  const [day, setDay] = useState(weekday[date.getDay()]);
+  
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const date = new Date();
+      setHours(date.getHours());
+      setMin(date.getMinutes());
+      setSec(date.getSeconds());
+      setDay(weekday[date.getDay()]);
+    }, 1000);
+    return () => clearInterval(timer);
+
+  });
+
+  console.log(day, hours, min, sec);
   return (
     <div className="App">
-     <Timer days = {days} hours = {hours} min = {min} sec = {sec}/>
+      <Timer day = {day} hours={hours} min={min} sec={sec} />
+      
     </div>
   );
 }
